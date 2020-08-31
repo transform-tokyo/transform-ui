35561892cd5bcd7790f5328444e06605e79f6026
intvergil <yan53074@gmail.com>
2020-08-31 17:32:08 +0900

    <feat>(upload)通过url管理oss文件

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  base: '/transform-ui',
+  publicPath: '/transform-ui/',
   mode: 'site',
   // more config: https://d.umijs.org/config
   menus: {
